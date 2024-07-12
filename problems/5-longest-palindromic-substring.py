0983be2522d0416235c89288e154a377565db37f
Wie Quach <wieq@Wies-MacBook-Pro-2.local>
2024-07-13 02:03:32 +0700

    feat: add new leetcode problem

--- problems/5-longest-palindromic-substring.py
+++ problems/5-longest-palindromic-substring.py
@@ -1,17 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if s[::-1] == s:
+        if len(s) <= 1:
             return s
 
-        start = 0
-        palind_dict = {}
-        for idx, char in enumerate(s):
-            longest_palind = ""
-            for jdx in range(idx, len(s)):
-                longest_palind += s[jdx]  
-                if longest_palind == longest_palind[::-1]:
-                    palind_dict[longest_palind] = len(longest_palind)
-        
-        result = sorted(palind_dict.items(), key=lambda x: x[1])[-1][0]
-        return result
-                    
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right
+
+        start, end = 0, 0
+
+        for i in range(len(s)):
+            left1, right1 = expand_from_center(i, i)
+            left2, right2 = expand_from_center(i, i + 1)
+            
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+
+        return s[start:end]
